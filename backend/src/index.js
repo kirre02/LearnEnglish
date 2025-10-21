12b0c210d2918af115d834125fbfae01e2ef0cf2
Marcus Gustavsson <marcus.gustavsson@iths.se>
2025-10-21 10:57:19 +0200

    fixed merge conflict

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,24 +6,18 @@ import wordRoutes from "./routes/wordRoutes.js";
 const app = express();
 const PORT = 9001;
 
-// Tillåt frontend (t.ex. Vite/Vue på port 5173)
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
 
 app.use(express.json());
-
-// Test-endpoint
 app.get("/hello", (req, res) => {
   res.send("hello world");
 });
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/words", wordRoutes);
-
-// Starta servern
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
 });
